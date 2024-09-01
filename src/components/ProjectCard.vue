96754754ec8af66fc91b8f2fa870f477d96db817
Farhan <farhan06.contact@gmail.com>
2024-09-01 13:02:33 +0600

    layout

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,15 +1,17 @@
 <template>
     <div class="card">
         <div class="card-text">
-            <h2 class="card-title">{{ pro.title }}</h2>
-            <p class="card-subtitle">{{ pro.tools }}</p>
-        </div>
-        <div class="card-img">
             <a class="card-link" :href="pro.link" target="_blank">
-                <img :src="require(`@/assets/img/${pro.img}`)" :alt="pro.title">
+                <h2 class="card-title">{{ pro.title }}</h2>
+                <p class="card-subtitle">{{ pro.tools }}</p>
             </a>
         </div>
-    </div>
+            <div class="card-img">
+                <a class="card-link" :href="pro.link" target="_blank">
+                    <img :src="require(`@/assets/img/${pro.img}`)" :alt="pro.title">
+                </a>
+            </div>
+        </div>
 </template>
 
 <script>
