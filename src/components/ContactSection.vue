0f3092fd1cd280baba26fa11fc49210c85634b79
Farhan <farhan06.contact@gmail.com>
2025-05-04 14:04:43 +0600

    notsure

--- src/components/ContactSection.vue
+++ src/components/ContactSection.vue
@@ -5,23 +5,26 @@
       <div class="contact__top">
         <h2 class="contact__top__text text_up">Share a cool fact or join me for a chat.</h2>
         <div class="contact__top__circle--text"></div>
-      </div> 
+      </div>
 
       <div class="contact_footer fade_simple">
-        <ul class=" contact_footer_links">
-            <li><a :href="'mailto:' + email" target="_blank" class="email"> {{ email }}<img class="arrow" src="@/assets/img/arrow.svg"/> </a></li>
-            <li><a href="https://dribbble.com/dev_farhan" target="_blank ">Dribbble</a></li>
-            <li><a href="https://github.com/farhan066" target="_blank">Github</a></li>
-            <li><a href="https://www.linkedin.com/in/farhan-ahmad-8194591b7" target="_blank">Linkedin</a></li>
+        <ul class="contact_footer_links">
+          <li><a :href="'mailto:' + email" target="_blank" class="email"> {{ email }}<img class="arrow"
+                src="@/assets/img/arrow.svg" /> </a></li>
+          <li><a href="https://dribbble.com/dev_farhan" target="_blank ">Dribbble</a></li>
+          <li><a href="https://github.com/farhan066" target="_blank">Github</a></li>
+          <li><a href="https://www.linkedin.com/in/farhan-ahmad-8194591b7" target="_blank">Linkedin</a></li>
         </ul>
-        <p class="contact_footer_copywrite">Designed and developed by me <br> ©{{ currentYear }} - All Rights Reserved</p>
+        <p class="contact_footer_copywrite">Designed and developed by me <br> ©{{ currentYear }} - All Rights Reserved
+        </p>
       </div>
     </div>
   </section>
-  
+
 </template>
 
 <script>
+
 
 export default {
   data() {
--- src/views/GamesView.vue
+++ src/views/GamesView.vue
@@ -133,4 +133,3 @@ export default {
 }
 
 </script>
-//'clip-path': 'polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)' full
